8b40591d475de3d318dc9b8ef269db977e9fa9f2
Owais Khan <owaisnoorj@gmail.com>
2024-10-10 18:29:59 +0500

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ export default function AboutPage() {
     <div className='h-screen w-full bg-pink-500'>
       <div >
         <h1 >you can read about myself</h1>
-        <p className=''>I am a passionate front-end web developer with a strong foundation in TypeScript Javascript, React. My work is driven by a commitment to building efficient, responsive, and user-friendly applications. I thrive on tackling challenges, whether it's optimizing a project for performance, resolving display issues across different devices, or navigating complex hosting environments. My focus on continuous learning and adaptability keeps me at the forefront of web technologies, enabling me to deliver high-quality solutions that meet the needs of users. I am always eager to expand my knowledge and take on new projects that push the boundaries of my skills.</p>
+        <p>I am a passionate front-end web developer with a strong foundation in TypeScript Javascript, React. My work is driven by a commitment to building efficient, responsive, and user-friendly applications. I thrive on tackling challenges, whether it&apos;s optimizing a project for performance, resolving display issues across different devices, or navigating complex hosting environments. My focus on continuous learning and adaptability keeps me at the forefront of web technologies, enabling me to deliver high-quality solutions that meet the needs of users. I am always eager to expand my knowledge and take on new projects that push the boundaries of my skills.</p>
         
       </div>
     </div>
